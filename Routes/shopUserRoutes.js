b7cf44c47eec8c3331778c573b36d35bb45841e3
Darius Duoba <darius.duoba@gmail.com>
2021-07-09 08:45:23 +0300

    get users route done

--- Routes/shopUserRoutes.js
+++ Routes/shopUserRoutes.js
@@ -2,8 +2,24 @@ const express = require('express');
 const ShopUser = require('../models/shopUser');
 const router = express.Router();
 
-router.get('/api/shop/shopusers/', (req, res) => {
-  res.json('there should be all users here');
+router.get('/api/shop/shopusers/', async (req, res) => {
+  try {
+    const users = await ShopUser.find();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// sukurti useri
+router.post('/api/shop/shopusers/new', async (req, res) => {
+  const newUser = new ShopUser(req.body);
+  try {
+    const result = await newUser.save();
+    res.json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
